c080413facc886675c79382ec1127860fc39fc72
Angkur Mondal <angkurmondal1998@gmail.com>
2024-04-28 17:16:02 +0600

    fixing memory leak

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css',
+  // providers: [ModalService]
+})
+export class ModalComponent {
+
+  @Input() modalID = '';
+
+  constructor(public modal: ModalService) {
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
+  }
+}
